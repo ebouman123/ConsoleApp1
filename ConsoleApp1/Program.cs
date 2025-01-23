5e15feadd312039dbb4dae4f3961c4f74294b03f
Evan Bouman <evan.bouman@gmail.com>
2025-01-23 10:34:06 -0600

    Initializing repo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Gordon";
+            Console.WriteLine($"Hi {name}");
+
+            string textAge = "9";
+            int age = Convert.ToInt32(textAge);
+            System.Console.WriteLine($"{name} is {age} years old");
+        }
+    }
+}
