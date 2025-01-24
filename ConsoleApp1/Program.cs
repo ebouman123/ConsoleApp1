c2953fc7b58f8b64ef7c8d03b724bfb0c303b8ea
Evan Bouman <evan.bouman@gmail.com>
2025-01-24 12:33:34 -0600

    odd/even exercise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,23 @@
     {
         static void Main(string[] args)
         {
-            string name = "Gordon";
-            Console.WriteLine($"Hi {name}");
+            // string name = "Gordon";
+            // Console.WriteLine($"Hi {name}");
 
-            string textAge = "9";
-            int age = Convert.ToInt32(textAge);
-            System.Console.WriteLine($"{name} is {age} years old");
+            // string textAge = "9";
+            // int age = Convert.ToInt32(textAge);
+            // System.Console.WriteLine($"{name} is {age} years old");
+
+            int first = 21;
+            int second = 12;
+
+            int remainder = first % second;
+
+            first = 923;
+
+            int newRemainder = first % second;
+
+            System.Console.WriteLine($"New Remainder = {newRemainder}");
         }
     }
 }
